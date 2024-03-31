e244f4a6d243fca64d41697942339021a8df2194
sumit joshi <47482270+Sumit-tech-joshi@users.noreply.github.com>
2024-03-31 15:37:06 -0700

    Add header style for mobile

--- design-system.js
+++ design-system.js
@@ -1,8 +1,15 @@
 
-// An event listener for toggling the sidebar menu
+// An event listener for toggling the sidebar menu in design system
 function handleToggleClick () {
   document.getElementById('hamburger-icon').classList.toggle("is-show-icon");
   document.getElementById('close-icon').classList.toggle("is-show-icon");
-  document.getElementById('sidebar-wrapper').classList.toggle("hide-sidebar-menu");
-  document.getElementById('nav-link-wrapper').classList.toggle("hide-sidebar-menu");
+  document.getElementById('sidebar-wrapper') && document.getElementById('sidebar-wrapper').classList.toggle("hide-sidebar-menu");
+  document.getElementById('nav-link-wrapper') && document.getElementById('nav-link-wrapper').classList.toggle("hide-sidebar-menu");
+}
+
+// An event listener for toggling the home menu
+function handleToggleHomeClick () {
+  document.getElementById('hamburger-icon').classList.toggle("is-show-icon");
+  document.getElementById('close-icon').classList.toggle("is-show-icon");
+  document.getElementById('nav-link-wrapper') && document.getElementById('nav-link-wrapper').classList.toggle("hide-top-menu");
 }
