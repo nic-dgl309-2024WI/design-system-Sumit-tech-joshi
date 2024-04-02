6d6464932d0f8bd4d322901967bebee1fd9ee543
sumit joshi <47482270+Sumit-tech-joshi@users.noreply.github.com>
2024-04-01 23:36:39 -0700

    Remove JS code for index file from design system

--- design-system.js
+++ design-system.js
@@ -13,35 +13,3 @@ function handleToggleHomeClick () {
   document.getElementById('close-icon').classList.toggle("is-show-icon");
   document.getElementById('nav-link-wrapper') && document.getElementById('nav-link-wrapper').classList.toggle("hide-top-menu");
 }
-
-
-// Function for pagination
-function handlePagination (pageNumber) {
-  let currentElement, pastElement;
-  if(pageNumber === 1) {
-    currentElement = document.getElementById(`c-our-pepople__section_page-${pageNumber}`);
-    pastElement = document.getElementById(`c-our-pepople__section_page-2`);
-  }
-  else {
-    currentElement = document.getElementById(`c-our-pepople__section_page-${pageNumber}`);
-    pastElement = document.getElementById(`c-our-pepople__section_page-1`);
-  }
-
-  pastElement.style.display = "none";
-  currentElement.style.display = "flex";
-}
-
-function handleTestimonialPagination (pageNumber) {
-  let currentElement, pastElement;
-  if(pageNumber === 1) {
-    currentElement = document.getElementById(`c-our-testimonial__section_page-${pageNumber}`);
-    pastElement = document.getElementById(`c-our-testimonial__section_page-2`);
-  }
-  else {
-    currentElement = document.getElementById(`c-our-testimonial__section_page-${pageNumber}`);
-    pastElement = document.getElementById(`c-our-testimonial__section_page-1`);
-  }
-
-  pastElement.style.display = "none";
-  currentElement.style.display = "flex";
-}
