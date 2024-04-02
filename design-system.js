bad55c38c6bf934e5212d5830b6d46502724ba97
sumit joshi <47482270+Sumit-tech-joshi@users.noreply.github.com>
2024-04-01 20:11:17 -0700

    Add testimonial section

--- design-system.js
+++ design-system.js
@@ -30,3 +30,18 @@ function handlePagination (pageNumber) {
   pastElement.style.display = "none";
   currentElement.style.display = "flex";
 }
+
+function handleTestimonialPagination (pageNumber) {
+  let currentElement, pastElement;
+  if(pageNumber === 1) {
+    currentElement = document.getElementById(`c-our-testimonial__section_page-${pageNumber}`);
+    pastElement = document.getElementById(`c-our-testimonial__section_page-2`);
+  }
+  else {
+    currentElement = document.getElementById(`c-our-testimonial__section_page-${pageNumber}`);
+    pastElement = document.getElementById(`c-our-testimonial__section_page-1`);
+  }
+
+  pastElement.style.display = "none";
+  currentElement.style.display = "flex";
+}
