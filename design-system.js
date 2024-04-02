089dbeed05db75ea45ab86ccb829a2d77bc9bae2
sumit joshi <47482270+Sumit-tech-joshi@users.noreply.github.com>
2024-04-02 09:21:35 -0700

    Add hero page

--- design-system.js
+++ design-system.js
@@ -13,3 +13,12 @@ function handleToggleHomeClick () {
   document.getElementById('close-icon').classList.toggle("is-show-icon");
   document.getElementById('nav-link-wrapper') && document.getElementById('nav-link-wrapper').classList.toggle("hide-top-menu");
 }
+
+
+// Funtion to copy code from clipbaard
+function copyToClipboard() {
+  var htmlCode = document.getElementById("htmlCode");
+  htmlCode.select();
+  document.execCommand("copy");
+  alert("HTML code copied to clipboard!");
+}
